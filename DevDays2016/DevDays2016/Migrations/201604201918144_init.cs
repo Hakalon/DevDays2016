4c57e45b59ad9d3f216dddd7ce3edd818e5a179b
Steve Lai <tf4ewg@hotmail.com>
2016-04-21 03:30:13 +0800

    安裝EntityFramwork、啟用Migration

---
+++ DevDays2016/DevDays2016/Migrations/201604201918144_init.cs
@@ -0,0 +1,47 @@
+namespace DevDays2016.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Question",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Response",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Tel = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.User");
+            DropTable("dbo.Response");
+            DropTable("dbo.Question");
+        }
+    }
+}
---
+++ DevDays2016/DevDays2016/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevDays2016.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        //一個問題會有多個回應
+        public List<Response> responses;
+        public User user;
+
+    }
+}
