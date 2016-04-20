0d64ad6e69ceb5f579e70ccf376f2b10835d7ab4
王思閎 <wangszuhung1@outlook.com>
2016-04-20 20:55:42 +0800

    更葉面

--- DevDays2016/DevDays2016/Controllers/HomeController.cs
+++ DevDays2016/DevDays2016/Controllers/HomeController.cs
@@ -8,20 +8,24 @@ namespace DevDays2016.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
         public ActionResult Agenda()
         {
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Index()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult QA()
+        {
+            return View();
+        
+        }
+        public ActionResult Answer()
+        {
             return View();
         }
     }
