3fff934d323c224cf949be346ed433b4c0a33d44
Steve Lai <tf4ewg@hotmail.com>
2016-04-20 14:06:54 +0800

    完成套版，準備填資料

--- DevDays2016/DevDays2016/Controllers/HomeController.cs
+++ DevDays2016/DevDays2016/Controllers/HomeController.cs
@@ -13,10 +13,8 @@ namespace DevDays2016.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Agenda()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
