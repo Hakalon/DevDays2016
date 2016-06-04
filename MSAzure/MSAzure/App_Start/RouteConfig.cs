e54d809e21a3f85e20cf9325ea25f4e13719ad2f
Steve Lai <tf4ewg@hotmail.com>
2016-06-04 14:27:41 +0800

    微修正

--- MSAzure/MSAzure/App_Start/RouteConfig.cs
+++ MSAzure/MSAzure/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace DevDays2016
+namespace MSAzure
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace DevDays2016
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Agenda", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
