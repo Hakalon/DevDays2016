2d2bc78c59135b691a6367dc05b2498671873eb6
Steve Lai <tf4ewg@hotmail.com>
2016-06-16 17:10:15 +0800

    完成Questionnaire(成功匯入資料至DB)

--- DevDays2016/DevDays2016/Controllers/HomeController.cs
+++ DevDays2016/DevDays2016/Controllers/HomeController.cs
@@ -33,9 +33,21 @@ namespace DevDays2016.Controllers
         {
             return View();
         }
-        public ActionResult Send()
+        public ActionResult Send(string place, string staff, string activity, string info, string other)
         {
-            return View("Thanks");
+            PostContext pTemp = new PostContext();
+            Questionnaire qTemp = new Questionnaire();
+
+            qTemp.Place = Convert.ToInt32(place);
+            qTemp.Staff = Convert.ToInt32(staff);
+            qTemp.Activity = Convert.ToInt32(activity);
+            qTemp.Information = info;
+            qTemp.Other = other;
+
+            pTemp.Questionnaires.Add(qTemp);
+            pTemp.SaveChanges();
+
+            return View("Agenda");
         }
     }
 }
