186053d242389a3026f86121de7bee91a14ade5f
Steve Lai <tf4ewg@hotmail.com>
2016-06-16 15:32:22 +0800

    完成現場報名(成功匯入Database)

--- DevDays2016/DevDays2016/Controllers/RegisterController.cs
+++ DevDays2016/DevDays2016/Controllers/RegisterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DevDays2016.Models;
 
 namespace DevDays2016.Controllers
 {
@@ -14,8 +15,20 @@ namespace DevDays2016.Controllers
             return View();
         }
 
-        public ActionResult Send()
+        public ActionResult Send(string name, string tel, string email,string nickName, string prof, string interestIn, string wantToMeet)
         {
+            PostContext ptemp = new PostContext();
+            User temp = new User();
+            temp.Name = name;
+            temp.NickName = nickName;
+            temp.Tel = tel;
+            temp.Email = email;
+            temp.Profession = prof;
+            temp.InterestIn = interestIn;
+            temp.WantToMeet = wantToMeet;
+            ptemp.Users.Add(temp);
+            ptemp.SaveChanges();
+
             return View("Signin");
         }
         public ActionResult SendSignin()
